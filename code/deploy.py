4cd13ceefa1adebf3e081dfeae6c69749cd3b1b6
yengsheng <yongsheng_choo@mymail.sutd.edu.sg>
2021-08-03 15:37:41 +0800

    Test python deploy

---
+++ code/deploy.py
@@ -0,0 +1,43 @@
+import os
+from ml_service.util.env_variables import Env
+
+from azureml.core.model import InferenceConfig
+from azureml.core import Workspace
+from azureml.core.model import Model
+
+from azureml.core.webservice import AciWebservice
+
+def main():
+    e = Env()
+    model_name = 'aviation_model'
+
+    ws = Workspace.get(
+        name=e.workspace_name,
+        subscription_id=e.subscription_id,
+        resource_group=e.resource_group,
+    )
+    print(ws.name, 'loaded.')
+
+    model = Model(ws, model_name)
+    inference_config = InferenceConfig(
+        environment = e,
+        conda_file="../AviationMLOps/conda_dependencies.yml",
+        entry_script="../AviationMLOps/scoring/score.py",
+    )  
+
+    deployment_config = AciWebservice.deploy_configuration(
+        cpu_cores=0.5, memory_gb=1, auth_enabled=True
+    )
+
+    service = Model.deploy(
+        ws,
+        "myservice",
+        [model],
+        inference_config,
+        deployment_config,
+        overwrite=True,
+    )
+    service.wait_for_deployment(show_output=True)
+
+if __name__ == '__main__':
+    main()
