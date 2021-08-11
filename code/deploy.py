2e3be7c383787583121c7d8672d2f0381a032b33
yengsheng <yongsheng_choo@mymail.sutd.edu.sg>
2021-08-11 12:21:30 +0800

    asdfsadgfdsg

--- code/deploy.py
+++ code/deploy.py
@@ -1,45 +1,43 @@
 import os
 from ml_service.util.env_variables import Env
 
-from azureml.core.model import InferenceConfig
-from azureml.core import Workspace
-from azureml.core.model import Model
-
 from azureml.core.webservice import AciWebservice
-from azureml.core.authentication import MsiAuthentication
-
+from azureml.core.model import InferenceConfig
+from azureml.core.model import Model
+from azureml.core import Workspace
 
 def main():
     e = Env()
-    model_name = 'aviation_model'
 
     ws = Workspace.get(
         name=e.workspace_name,
         subscription_id=e.subscription_id,
         resource_group=e.resource_group
     )
-    print(ws.name, 'loaded.')
 
-    model = Model(ws, model_name)
-    inference_config = InferenceConfig(
-        environment = e,
-        conda_file="../AviationMLOps/conda_dependencies.yml",
-        entry_script="../AviationMLOps/scoring/score.py",
-    )  
+    # Set path for scoring script and environment files
+    script_file = os.path.join("./code/exp_folder", "score.py")
+    env_file = os.path.join("./code/exp_folder", "env.yml")
 
-    deployment_config = AciWebservice.deploy_configuration(
-        cpu_cores=0.5, memory_gb=1, auth_enabled=True
-    )
+    # Configure the scoring environment
+    inference_config = InferenceConfig(runtime= "python",
+                                    entry_script=script_file,
+                                    conda_file=env_file)
 
-    service = Model.deploy(
-        ws,
-        "myservice",
-        [model],
-        inference_config,
-        deployment_config,
-        overwrite=True,
-    )
-    service.wait_for_deployment(show_output=True)
+    deployment_config = AciWebservice.deploy_configuration(cpu_cores = 1, memory_gb = 1)
+
+    service_name = "aviation-service"
+    model_name = "aviation_model"
+    model = ws.models[model_name]
+    print(model.name, 'version', model.version)
+
+    service = Model.deploy(ws, service_name, [model], inference_config, deployment_config)
+
+    service.wait_for_deployment(True)
+    print(service.state)
+
+    print(service.get_logs())
+
 
 if __name__ == '__main__':
     main()
